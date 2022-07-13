59e655c4a82026c9ba2ab2d6c3cbe426d7959f7e
jdl <1265105759@qq.com>
2022-07-13 17:05:57 +0800

    threadLocal测试

--- src/main/java/com/shebao/test/springRabbitMq/controller/SendMessageController.java
+++ src/main/java/com/shebao/test/springRabbitMq/controller/SendMessageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -27,14 +28,14 @@ public class SendMessageController {
 
     @GetMapping("/sendMsg/{message}")
     public void sendMsg (@PathVariable("message") String message){
-        log.info("当前时间为:{},发送的消息为:{}",new Date().toString(),message);
+        log.info("当前时间为:{},发送的消息为:{}", new SimpleDateFormat("yyyy-MM-dd:HH-mm-ss").format(new Date()),message);
         rabbitTemplate.convertAndSend(RabbitMqConstant.X_EXCHANGE,RabbitMqConstant.X_ROUTING_KEY_QA,"死信时间为10秒的消息："+message);
         rabbitTemplate.convertAndSend(RabbitMqConstant.X_EXCHANGE,RabbitMqConstant.X_ROUTING_KEY_QB,"死信时间为40秒的消息："+message);
     }
 
     @GetMapping("/sendMsg/{message}/{delayTime}")
     public void sendDelayTimeMsg(@PathVariable("message") String message,@PathVariable("delayTime") String delayTime){
-        log.info("当前时间为:{},发送的消息为:{},延迟时长为:{}",new Date().toString(),message,delayTime);
+        log.info("当前时间为:{},发送的消息为:{},延迟时长为:{}",new SimpleDateFormat("yyyy-MM-dd:HH-mm-ss").format(new Date()),message,delayTime);
         rabbitTemplate.convertAndSend(RabbitMqConstant.X_EXCHANGE,RabbitMqConstant.X_ROUTING_KEY_QC,message,messagePostProcessor->{
             // 设置发送消息的时候，延迟时间
             messagePostProcessor.getMessageProperties().setExpiration(delayTime);
