d34987dc2567325422c79d3ea7a3150df818114a
jdl <1265105759@qq.com>
2022-08-26 17:26:46 +0800

    测试

--- src/main/java/com/shebao/test/test/EasyExcelTest.java
+++ src/main/java/com/shebao/test/test/EasyExcelTest.java
@@ -8,10 +8,14 @@ import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.alibaba.excel.write.metadata.fill.FillConfig;
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.*;
 
 @Slf4j
@@ -19,7 +23,15 @@ public class EasyExcelTest {
     // C:\Users\EDY\Desktop\test.xlsx
     public static void main(String[] args) {
 //        simpleWriteExcel();
-        complexFillExcel();
+//        complexFillExcel();
+        TJTBQingFeng tjtbQingFeng = new EasyExcelTest.TJTBQingFeng();
+        tjtbQingFeng.setGrzh("1111");
+        tjtbQingFeng.setDwjcbl("5");
+        tjtbQingFeng.setGrjcbl("5");
+        tjtbQingFeng.setXm("5555");
+        tjtbQingFeng.setZjhm("320804199810261910");
+        tjtbQingFeng.setJcjs("5000");
+        EasyExcelTest.fillData(Collections.singletonList(tjtbQingFeng));
     }
 
     /**
@@ -57,6 +69,34 @@ public class EasyExcelTest {
 
 
     }
+
+    @Data
+    static class  TJTBQingFeng{
+        private String grzh;
+        private String xm;
+        private String zjhm;
+        private String jcjs;
+        private String dwjcbl;
+        private String grjcbl;
+    }
+
+    // 填充数据
+    private static void fillData(List<TJTBQingFeng> fillList){
+        log.info("fillExecl start {}", JSON.toJSONString(fillList));
+        InputStream systemResourceAsStream = ClassLoader.getSystemResourceAsStream("templates/调基调比启封【2022-08-26】.xlsx");
+        File tempFile = new File("C:\\Users\\EDY\\Desktop\\EasyExcel-"+System.currentTimeMillis()+".xlsx");
+        ExcelWriter excelWriter = EasyExcel.write(tempFile).withTemplate(systemResourceAsStream).build();
+        FillConfig fillConfig = FillConfig.builder().forceNewRow(true).build();  // 自动换行
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        excelWriter.fill(fillList,fillConfig,writeSheet);
+        try {
+            FileUtil.writeFromStream(new FileInputStream(tempFile),tempFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        excelWriter.finish();
+    }
+
 
 
     /**
